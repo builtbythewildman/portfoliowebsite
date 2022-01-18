e562e99e38c6935c5ec931bc07fdf73fd38d3d98
Xander <xwernecke@gmail.com>
2022-01-18 22:26:43 +0100

    structural build
    added structure for the entire website

---
+++ assets/js/app.js
@@ -0,0 +1,67 @@
+//slider JS 
+new fullpage('#fullpage', {
+  //menu
+	licenseKey: null,
+  menu: '#menu',
+	lockAnchors: false,
+	anchors:['home', 'aboutme','projects','contact'],
+	navigation: true,
+	navigationPosition: 'left',
+	navigationTooltips: ['HOME', 'ABOUT ME','PROJECTS','CONTACT'],
+	showActiveTooltip: false,
+	slidesNavigation: false,
+	slidesNavPosition: 'bottom',
+  //scrolling
+  easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+	loopBottom: false,
+	loopTop: false,
+  animateAnchor: true,
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  sectionsColor: ['#394648', '#B9BAA3', '#D6D5C9', '#A22C29'],
+  lazyLoading: true,
+});
+//methods
+fullpage_api.setAllowScrolling(true);
+
+//chart js
+const labels = [
+  
+	'Bootstrap',
+  'JavaScript',
+  'Git Version Control',
+	'Shopify Liquid',
+	'CSS',
+	'HTML',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Skills Visualization',
+    backgroundColor: [
+      '#6699CC',
+      '#FFF275',
+      '#4D243D',
+      '#A22C29',
+      '#004777',
+			'#FF7700'
+    ],
+    data: [8, 6, 6.2, 6, 7.8, 8.8] ,
+  }]
+};
+
+const config = {
+  type: 'polarArea',
+  data: data,
+  options: {}
+};
+const myChart = new Chart(
+	document.getElementById('myChart'),
+	config
+);
+
+//init AOS 
+AOS.init();
